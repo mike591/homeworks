1aa6ed3d4202f1a14b4d0569037ce7237e6dad59
Michael <mikemach123@gmail.com>
2016-11-18 08:06:02 -0800

    wrote the Map class

--- W1D5/map.rb
+++ W1D5/map.rb
@@ -1,6 +1,31 @@
 class Map
+  def initialize
+    @map = []
+  end
 
+  def assign(key, value)
+    location = lookup(key)
+    if location != nil
+      @map[location][1] = value
+    else
+      @map << [key,value]
+    end
+  end
 
+  def lookup(key)
+    location = nil
+    @map.each_with_index do |el, idx|
+      location = idx if el[0] == key
+    end
+    return location
+  end
 
+  def remove(key)
+    location = lookup(key)
+    @map.slice!(location,1)
+  end
 
+  def show
+    @map
+  end
 end
